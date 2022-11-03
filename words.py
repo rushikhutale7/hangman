b8d1b958a7d313840e627449210c1a1e3cea20d4
rushikhutale7 <117240483+rushikhutale7@users.noreply.github.com>
2022-11-03 16:10:30 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,10 @@
+words = ["aback","abaft","abandoned","abashed","aberrant","abhorrent","abiding","blushing","boiling","boorish","bored","boring",\
+ "bouncy","boundless","brainy","brash","brave","brawny","breakable","breezy","brief","bright","bright","broad","broken","brown","bumpy","burly",\
+ "cumbersome","curious","curly","curved","curvy","cut","cute","cute","cynical","daffy","daily","damaged","damaging","damp","dangerous","dapper",\
+  "dark","dashing","dazzling","dead","deadpan","deafening","dear","debonair","decisive","decorous","deep","deeply","defeated","defective","defiant",\
+ "excellent","excited","exciting","exclusive","exotic","expensive","extra-large","extra-small","exuberant","exultant","fabulous","faded","faint",\
+ "fair","faithful","fallacious","false","familiar","famous","fanatical","fancy","fantastic","far","macabre","macho","maddening","madly","magenta",\
+"magical","magnificent","majestic","makeshift","male","malicious","mammoth","maniacal","many","marked","massive","married","marvelous","material",\
+ "materialistic","mature","mean","measly","meaty","medical","meek","mellow","melodic","melted","merciful","mere","messy","mighty","military","milky","mindless"]
+
+
